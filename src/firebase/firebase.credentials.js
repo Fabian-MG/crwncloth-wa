dff68298293fda7417a5316a3845d3a0112a4c38
Fabian-MG <mirandafabian1510@gmail.com>
2020-07-21 12:25:12 -0500

    Firebase configuration

---
+++ src/firebase/firebase.credentials.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC8K7zd7prWBzAU2z395Uq2X5TAsC6aMzM",
+    authDomain: "crown-db-24be0.firebaseapp.com",
+    databaseURL: "https://crown-db-24be0.firebaseio.com",
+    projectId: "crown-db-24be0",
+    storageBucket: "crown-db-24be0.appspot.com",
+    messagingSenderId: "603810953046",
+    appId: "1:603810953046:web:afaee7a2b6bc9090",
+  };
+  
